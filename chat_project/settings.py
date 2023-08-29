1008d9bf2bbfbaa9d8014ea697cb1373270da630
varun kumar <varun.kumar@zenatix.com>
2023-08-29 22:40:49 +0530

    Add django-allauth and making /chat the homepage

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #all auth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +137,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
--- chat_project/urls.py
+++ chat_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("chat/", include("chat_app.urls")),
+    path("", include("chat_app.urls")),
+    path('accounts/', include('allauth.urls')),
 ]
